2bc5ebb41cffdc4bdf087a4912f3fc806bec01eb
peaceplayer0722 <kohei722@i.softbank.jp>
2022-05-26 23:24:45 +0900

    コメントを追加

--- MRR/mymath.py
+++ MRR/mymath.py
@@ -4,29 +4,29 @@ import numpy as np
 import numpy.typing as npt
 
 
-def is_zero(x: np.float_, y: np.float_) -> np.bool_:
+def is_zero(x: np.float_, y: np.float_) -> np.bool_:        #x/yの小数部分<0.1：Trueを返す　それ以外：False
     """
     x > y > 0
     """
-    result: np.float_ = x / y - np.floor(x / y, dtype=np.float_)
+    result: np.float_ = x / y - np.floor(x / y, dtype=np.float_)    
     return result < 0.1
 
 
-def lcm(xs: npt.NDArray[np.float_]) -> np.float_:
+def lcm(xs: npt.NDArray[np.float_]) -> np.float_:   #xs:ndarray[float] の実数範囲に拡張した最小公倍数を返す
     return reduce(_lcm, xs)
 
 
-def mod(x: np.float_, y: np.float_) -> np.float_:
+def mod(x: np.float_, y: np.float_) -> np.float_:           #実数範囲でのmodを計算
     result: np.float_ = x - y * np.floor(x / y)
     return result
 
 
-def _gcd(x: np.float_, y: np.float_) -> np.float_:
+def _gcd(x: np.float_, y: np.float_) -> np.float_:          #gcd(x,y)を計算
     """
     x > y
     """
     n = 0
-    while y != 0 and not is_zero(x, y) and n < 10:
+    while y != 0 and not is_zero(x, y) and n < 10:          #実数範囲でのユークリッドの互除法
         x, y = y, mod(x, y)
         n += 1
 
@@ -36,7 +36,7 @@ def _gcd(x: np.float_, y: np.float_) -> np.float_:
         return x
 
 
-def _lcm(x: np.float_, y: np.float_) -> np.float_:
+def _lcm(x: np.float_, y: np.float_) -> np.float_:          #最小公倍数を計算
     if x > y:
         return x * y / _gcd(x, y)
     else:
--- MRR/transfer_function.py
+++ MRR/transfer_function.py
@@ -3,19 +3,22 @@ import numpy.typing as npt
 
 
 def simulate_transfer_function(
-    wavelength: npt.NDArray[np.float_],
-    L: npt.ArrayLike,
-    K: npt.ArrayLike,
-    alpha: float,
-    eta: float,
-    n_eff: float,
-    n_g: float,
-    center_wavelength: float,
-) -> npt.NDArray[np.float_]:
+    wavelength: npt.NDArray[np.float_],     #サンプリング波長
+    L: npt.ArrayLike,                       #リング周長のリスト
+    K: npt.ArrayLike,                       #結合率のリスト
+    alpha: float,                           #電力透過率
+    eta: float,                             #結合損
+    n_eff: float,                           #実行屈折率
+    n_g: float,                             #群屈折率
+    center_wavelength: float,               #中心波長
+) -> npt.NDArray[np.float_]:                #伝達関数のリストを出力
     y: npt.NDArray[np.float_] = 20 * np.log10(np.abs(_D(L, K, alpha, wavelength, eta, n_eff, n_g, center_wavelength)))
     return y.reshape(y.size)
 
-
+'''
+(K_n:float, 結合損:float)
+-> C(K):2×2行列を計算
+'''
 def _C(K_k: float, eta: float) -> npt.NDArray[np.float_]:
     C: npt.NDArray[np.float_] = (
         1
@@ -24,7 +27,11 @@ def _C(K_k: float, eta: float) -> npt.NDArray[np.float_]:
     )
     return C
 
-
+'''
+(電力透過率a:float,リング周長 L_n:float, 波長 wavelength:ndarray[float], 
+ 実行屈折率 n_eff:float,群屈折率 n_g:float, 中央波長center_wavelength:float)
+->R(a,L,λ):2×2行列
+'''
 def _R(
     a_k: float,
     L_k: float,
@@ -48,19 +55,19 @@ def _R(
 
 
 def _M(
-    L: npt.ArrayLike,
-    K: npt.ArrayLike,
-    alpha: float,
-    wavelength: npt.NDArray[np.float_],
-    eta: float,
-    n_eff: float,
-    n_g: float,
-    center_wavelength: float,
-) -> npt.NDArray[np.float_]:
+    L: npt.ArrayLike,                       #リング周長のリスト
+    K: npt.ArrayLike,                       #結合率のリスト
+    alpha: float,                           #伝搬損失係数
+    wavelength: npt.NDArray[np.float_],     #サンプリング波長
+    eta: float,                             #結合損
+    n_eff: float,                           #実行屈折率
+    n_g: float,                             #群屈折率
+    center_wavelength: float,               #中心波長
+) -> npt.NDArray[np.float_]:                #伝達行列:2×2行列
     L_: npt.NDArray[np.float_] = np.array(L)[::-1]
     K_: npt.NDArray[np.float_] = np.array(K)[::-1]
     a: npt.NDArray[np.float_] = np.exp(-alpha * L_)
-    product = np.identity(2)
+    product = np.identity(2)                #2×2の単位行列を生成
     for K_k, a_k, L_k in zip(K_[:-1], a, L_):
         product = np.dot(product, _C(K_k, eta))
         product = np.dot(product, _R(a_k, L_k, wavelength, n_eff, n_g, center_wavelength))
@@ -69,14 +76,14 @@ def _M(
 
 
 def _D(
-    L: npt.ArrayLike,
-    K: npt.ArrayLike,
-    alpha: float,
-    wavelength: npt.NDArray[np.float_],
-    eta: float,
-    n_eff: float,
-    n_g: float,
-    center_wavelength: float,
-) -> npt.NDArray[np.float_]:
+    L: npt.ArrayLike,                          #リング周長のリスト
+    K: npt.ArrayLike,                          #結合率のリスト
+    alpha: float,                              #伝搬損失係数
+    wavelength: npt.NDArray[np.float_],        #サンプリング波長
+    eta: float,                                #結合損
+    n_eff: float,                              #実行屈折率
+    n_g: float,                                #群屈折率
+    center_wavelength: float,                  #中心波長
+) -> npt.NDArray[np.float_]:                   #input-drop間の伝達関数を出力             
     D: npt.NDArray[np.float_] = 1 / _M(L, K, alpha, wavelength, eta, n_eff, n_g, center_wavelength)[0, 0]
     return D
